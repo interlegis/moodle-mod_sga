7c353bde15b763d03d010c048cbf3c6e21844056
Matheus Garcia <matheusg@senado.leg.br>
2021-09-27 14:58:54 -0300

    Início de desenvolvimento do plugin de integração com o SGA

---
+++ externallib.php
@@ -0,0 +1,42 @@
+<?php
+
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ *
+ */
+require_once($CFG->libdir . "/externallib.php");
+
+class mod_sga_external extends external_api {
+
+   /* public static function get_usage_statistics() {
+        global $USER;
+
+        return '{"cursos": "10", "alunos":"20", "certificados":"12"}';
+    }
+
+    public static function get_usage_statistics_parameters() {
+        return new external_function_parameters(
+                array()
+        );
+    }
+
+    public static function get_usage_statistics_returns() {
+        return new external_value(PARAM_TEXT, 'JSON with statistics of this installation');
+    }
+
+    */
+
+}
+
---
+++ version.php
@@ -0,0 +1,4 @@
+<?php
+$plugin->version = 2021092701;
+$plugin->requires = 2017110800;
+$plugin->component = 'mod_sga';
